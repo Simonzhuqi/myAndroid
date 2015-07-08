4842018598cce62076cb5f4274117835d38e48a9
simonzhuqi <312282860@qq.com>
2015-07-08 13:40:35 +0800

    sidermenu

---
+++ 自定义UI/src/com/itrun/zhuqi/ui/MainActivity.java
@@ -0,0 +1,42 @@
+package com.itrun.zhuqi.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.itrun.mywidget.R;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private TextView siderMeun;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		siderMeun = (TextView)findViewById(R.id.textView1);
+		siderMeun.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.textView1:
+			startActivity(new Intent(MainActivity.this,SiderMeunActivity.class));
+			break;
+		}
+	}
+
+}
